90670cad34446122ac8fd38970213b890c7392b4
Yakiel <codey4k@gmail.com>
2016-11-12 00:01:28 +0000

    Project start

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Persona from './persona'
+
+const y4k = new Persona('Yakiel')
+
+y4k.saludo()
---
+++ src/persona.js
@@ -0,0 +1,9 @@
+export default class Persona {
+    constructor(nombre){
+        this.nombre = nombre
+    }
+    
+    saludo(){
+        document.write('Hola, ' + this.nombre + '...')
+    }
+}
